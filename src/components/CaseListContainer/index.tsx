dda18d5eee32a3ac44d3457da6023f237142a0f3
oscar <oscar.phang@forefront.com.my>
2020-04-11 17:52:34 +0800

    fix date search problem

--- src/components/CaseListContainer/index.tsx
+++ src/components/CaseListContainer/index.tsx
@@ -40,7 +40,7 @@ const CaseListContainer = () => {
                     </Select>
                     </Box>
                     <Box flex="1">
-                    <Select styles={customSelectStyle} value={QUERY_RANGE.find(elem=>elem.value===String(currentRange))} onChange={(item:any)=>setCurrentRange(item.value)}  options={QUERY_RANGE}></Select>
+                    <Select styles={customSelectStyle} value={QUERY_RANGE.find(elem=>elem.value===String(currentRange))} onChange={(item:any)=>setCurrentRange(Number(item.value))}  options={QUERY_RANGE}></Select>
                     </Box>
                     </Flex>
                     <CaseList country={currentCountry!==""?currentCountry:country} range={currentRange+1}></CaseList>
